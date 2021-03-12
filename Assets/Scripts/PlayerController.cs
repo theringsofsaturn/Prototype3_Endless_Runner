7cca03f23328f0fae1840d05363ea0020bed81ce
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-03-12 03:34:04 +0100

    Rigid body add force

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody PlayerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRb = GetComponent<Rigidbody>();
+        PlayerRb.AddForce(Vector3.up * 400);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
