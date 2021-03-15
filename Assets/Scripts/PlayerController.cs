14ad75f611c15ac599e1db1a5c28bc0a70f7b0f9
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-03-15 17:36:28 +0100

    Game Over Trigger Added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver;
 
 
     // Start is called before the first frame update
@@ -15,6 +16,7 @@ public class PlayerController : MonoBehaviour
     {
         PlayerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
+        
         
     }
 
@@ -31,6 +33,15 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        } 
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+
+        }
     }
 }
