0eb58a7f96ab43233b02611df27db4b6b6b4fa99
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-03-17 00:38:44 +0100

    Add a dirt splatter particle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody PlayerRb;
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver;
-    private Animator playerAnim;
+    
 
 
     // Start is called before the first frame update
@@ -31,6 +35,7 @@ public class PlayerController : MonoBehaviour
             PlayerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
         }
         
     }
@@ -40,6 +45,7 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
         } 
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
@@ -47,8 +53,8 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Game Over!");
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
-
-
+            explosionParticle.Play();
+            dirtParticle.Stop();
         }
     }
 }
